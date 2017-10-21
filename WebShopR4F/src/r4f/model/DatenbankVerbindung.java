37239bdf9bded036ad2b02742e19135351d41f30
iluvater <ture.kuesener@gmx.de>
2017-10-21 12:53:10 +0200

    Insert and GetArticle

--- WebShopR4F/src/r4f/model/DatenbankVerbindung.java
+++ WebShopR4F/src/r4f/model/DatenbankVerbindung.java
@@ -96,7 +96,6 @@ public class DatenbankVerbindung {
 
 				preparedStatement.executeUpdate();
 
-
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
@@ -105,6 +104,7 @@ public class DatenbankVerbindung {
 
 	/**
 	 * This method select a Benutzer from the database selected by the email
+	 * 
 	 * @param email
 	 *            Email of the user that should be get from the database.
 	 * @return the user with all Attributes that are stored in the database
@@ -148,9 +148,13 @@ public class DatenbankVerbindung {
 
 	/**
 	 * This method creates a new article in the database
-	 * @param artikel the article that should be created
+	 * 
+	 * @param artikel
+	 *            the article that should be created
+	 * @return return the id of the created article if the articel was not
+	 *         created the method return -1
 	 */
-	public void createArtikelInDB(Artikel artikel) {
+	public int createArtikelInDB(Artikel artikel) {
 		conn = getInstance();
 
 		if (conn != null) {
@@ -158,29 +162,146 @@ public class DatenbankVerbindung {
 			try {
 
 				PreparedStatement preparedStatement = conn.prepareStatement(
-						"INSERT INTO `Artikel` (`id`, `bezeichung`, `beschreibung`, `groesse`, `preis`, `hersteller`, `farbe`, `erfassungsdatum`, `kategorie`) "
-						+ "VALUES (NULL, ?, ?, ?, ?, ?, ?, null, ?)");
-				 preparedStatement.setString(1, artikel.getBezeichnung());
-				 preparedStatement.setString(2, artikel.getBeschreibung());
-				 preparedStatement.setInt(3, artikel.getGroesse());
-				 preparedStatement.setDouble(4, artikel.getPreis());
-				 preparedStatement.setString(5, artikel.getHersteller());
-				 preparedStatement.setString(6, artikel.getFarbe());
-				 preparedStatement.setString(7, artikel.getKategorie());
-				
-				 preparedStatement.executeUpdate();		
-				 
-				
-				 
+						"INSERT INTO `Artikel` (`id`, `bezeichnung`, `beschreibung`, `groesse`, `preis`, `hersteller`, `farbe`, `erfassungsdatum`, `kategorie`) "
+								+ "VALUES (NULL, ?, ?, ?, ?, ?, ?, null, ?)",
+						Statement.RETURN_GENERATED_KEYS);
+				preparedStatement.setString(1, artikel.getBezeichnung());
+				preparedStatement.setString(2, artikel.getBeschreibung());
+				preparedStatement.setInt(3, artikel.getGroesse());
+				preparedStatement.setDouble(4, artikel.getPreis());
+				preparedStatement.setInt(5, getHerstellerId(artikel.getHersteller()));
+				preparedStatement.setString(6, artikel.getFarbe());
+				preparedStatement.setInt(7, getKategorieId(artikel.getKategorie()));
 
+				int zeilen = preparedStatement.executeUpdate();
+
+				if (zeilen != 0) {
+					ResultSet result = preparedStatement.getGeneratedKeys();
+					if (result.next()) {
+						return result.getInt(1);
+					} else {
+						return -1;
+					}
+				} else {
+					return -1;
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return -1;
+			}
+		} else {
+			return -1;
+		}
+	}
+	
+	
+	/**
+	 * This method selects an article from the database 
+	 * @param id the id of the article that should be selected
+	 * @return returns the selected article if there is no article with this id in the database
+	 */
+	public Artikel getArtikel(int id) {
+		Artikel artikel = null;
+
+		conn = getInstance();
+		if (conn != null) {
+			// Anfrage-Statement erzeugen.
+			Statement query;
+			try {
+				query = conn.createStatement();
+
+				// Ergebnistabelle erzeugen und abholen.
+				String sql = "SELECT a.id, a.bezeichnung, a.beschreibung, a.groesse, a.preis, h.bezeichnung as hersteller, a.farbe, a.erfassungsdatum, k.bezeichnung as kategorie"
+						+ " FROM artikel AS a INNER JOIN kategorien AS k INNER JOIN hersteller AS h"
+						+ " WHERE a.kategorie = k.id AND a.hersteller = h.id AND a.id = " + id;
+				ResultSet result = query.executeQuery(sql);
+
+				// Ergebnissätze durchfahren.
+				while (result.next()) {
+					id = result.getInt("id");
+					String bezeichnung = result.getString("bezeichnung");
+					String beschreibung = result.getString("beschreibung");
+					int groesse = result.getInt("groesse");
+					double preis = result.getDouble("preis");
+					String hersteller = result.getString("hersteller");
+					String farbe = result.getString("farbe");
+					Date erfassungsdatum = result.getDate("erfassungsdatum");
+					String kategorie = result.getString("kategorie");
+					
+					artikel = new Artikel(id, bezeichnung, beschreibung, groesse, preis, hersteller, farbe, erfassungsdatum, kategorie);
+				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
+		return artikel;
 	}
-	
-	public Artikel getArtikel(){
-		return null;
+
+	/**
+	 * This method gets the id to the string of a kategorie
+	 * 
+	 * @param kategorie
+	 *            kategorie for which the id should be selected from the
+	 *            database
+	 * @return the id of the kategorie
+	 */
+	public int getKategorieId(String kategorie) {
+		int id = -1;
+		conn = getInstance();
+
+		if (conn != null) {
+			// Anfrage-Statement erzeugen.
+			Statement query;
+			try {
+				query = conn.createStatement();
+
+				// Ergebnistabelle erzeugen und abholen.
+				String sql = "SELECT id FROM kategorien WHERE bezeichnung='" + kategorie + "'";
+				ResultSet result = query.executeQuery(sql);
+
+				// Ergebnissätze durchfahren.
+				if (result.next()) {
+					id = result.getInt("id");
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return id;
+	}
+
+	/**
+	 * This method selects the id of a Hersteller
+	 * 
+	 * @param hersteller
+	 *            the hersteller for whicht the id should be selected
+	 * @return the id of the hersteller
+	 */
+	public int getHerstellerId(String hersteller) {
+		int id = -1;
+		conn = getInstance();
+
+		if (conn != null) {
+			// Anfrage-Statement erzeugen.
+			Statement query;
+			try {
+				query = conn.createStatement();
+
+				// Ergebnistabelle erzeugen und abholen.
+				String sql = "SELECT id FROM hersteller WHERE bezeichnung='" + hersteller + "'";
+				ResultSet result = query.executeQuery(sql);
+
+				// Ergebnissätze durchfahren.
+				if (result.next()) {
+					id = result.getInt("id");
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return id;
 	}
 
 }
--- WebShopR4F/src/r4f/model/TestInsert.java
+++ WebShopR4F/src/r4f/model/TestInsert.java
@@ -1,18 +1,17 @@
 package r4f.model;
 
-import java.sql.Date;
-
 public class TestInsert {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		DatenbankVerbindung db = new DatenbankVerbindung();
 				
-		Benutzer b = new Benutzer(1, "vorname", "nachname", "email3@test.com", new Date(1),  "password", "strasse", "hausnummer", "12345", "Stadt", "Herr");
+		Artikel a = new Artikel("Bezeichung", "beschreibung", 45, 49.99, "Hummel", "farbe", "Herren");
 		
-		db.CreateBenutzerInDB(b);
-		b = db.getBenutzer("email3@test.com");
-		System.out.println(b.getId());
+		int id = db.createArtikelInDB(a);
+		
+		a = db.getArtikel(id);
+		System.out.println(a.getErfassungsdatum().toString());
 		
 	}
 
