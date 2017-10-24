0873411f6a979f3f633beca742b8e24081a3dfc5
iluvater <ture.kuesener@gmx.de>
2017-10-24 20:17:55 +0200

    Anpassungen Fehlermeldungen und Umstellung Fehlermeldungen auf Laden der Fehlermeldungen aus conf

--- WebShopR4F/src/r4f/model/ErrorMessage.java
+++ WebShopR4F/src/r4f/model/ErrorMessage.java
@@ -1,10 +1,10 @@
 package r4f.model;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  *  Class that represents a error Message
@@ -16,96 +16,27 @@ public class ErrorMessage {
 	private String errorMessage;
 	
 	/**
-	 * 
+	 * this Constructor selects the errorMessage of the errorCode from the file errors.conf. 
 	 * @param errorCode Code of the Error Message
 	 */
 	public ErrorMessage(int errorCode){
 		this.setErrorCode(errorCode);
-		switch (errorCode) {
-		case 100:
-			this.errorMessage= "Bitte geben Sie Ihren Vornamen ein.";
-			break;
-		case 101:
-			this.errorMessage= "Während der Registrierung ist ein Fehler aufgetreten. Bitte Versuchen Sie es erneut";
-			break;
-		case 102:
-			this.errorMessage= "Bitte Geben Sie das Datum im richtigen Format DD.MM.JJJJ oder JJJJ-MM-DD ein.";
-			break;
-		case 103:
-			this.errorMessage= "Diese E-Mail ist bereits registriert. Melden Sie sich an.";
-			break;
-		case 104:
-			this.errorMessage= "Bitte wählen Sie eine gültige Anrede aus.";
-			break;
-		case 105:
-			this.errorMessage= "Bitte geben Sie eine gültige Postleitzahl ein.";
-			break;
-		case 106:
-			this.errorMessage= "Bitte geben Sie ein gültige E-Mail ein.";
-			break;
-		case 107:
-			this.errorMessage= "Bitte geben Sie Ihren Nachnamen ein.";
-			break;
-		case 108:
-			this.errorMessage= "Bitte geben Sie ihre E-Mail ein.";
-			break;
-		case 109:
-			this.errorMessage= "Bitte geben Sie ein Passwort ein.";
-			break;
-		case 110:
-			this.errorMessage= "Bitte geben Sie eine Straße ein.";
-			break;
-		case 111:
-			this.errorMessage= "Bitte geben Sie eine Hausnummer ein.";
-			break;
-		case 112:
-			this.errorMessage= "Bitte geben Sie einen Ort ein.";
-			break;
-		case 113:
-			this.errorMessage= "Bitte geben Sie ihr Geburtsdatum ein.";
-			break;
-		case 114:
-			this.errorMessage= "Das Passwort oder die E-Mail ist flasch.";
-			break;
-		case 115:
-			this.errorMessage= "Bitte geben Sie einen Preis ein.";
-			break;
-		case 116:
-			this.errorMessage= "Bitte geben Sie eine Größe ein.";
-			break;
-		case 117:
-			this.errorMessage= "Bitte geben Sie eine Bezeichnung ein.";
-			break;
-		case 118:
-			this.errorMessage= "Bitte geben Sie eine Beschreibung ein.";
-			break;
-		case 119:
-			this.errorMessage= "Bitte geben Sie einen Hersteller ein.";
-			break;
-		case 120:
-			this.errorMessage= "Bitte geben Sie eine Farbe ein.";
-			break;
-		case 121:
-			this.errorMessage= "Bitte geben Sie eine Kategorie ein.";
-			break;
-		case 122:
-			this.errorMessage= "Es ist ein Fehler während der Artikelerfassung aufgetreten. Bitte versuchen Sie es später erneut.";
-			break;
-		default:
-			this.errorMessage= "Unbekannter Fehler.";
-			break;
-		}
-		
-		BufferedReader bReader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("./WebContent/erros.ini")));
-		String zeile="";
-		
-		
+
 		try {
-			while((zeile= bReader.readLine() )!= null){
+			BufferedReader bReader = new BufferedReader(new InputStreamReader(ErrorMessage.class.getResourceAsStream("errors.conf")));
+			String zeile="";	
+			
+			boolean temp = true;
+			while((zeile= bReader.readLine() )!= null && temp){
 				String split[] = zeile.split(";");
 				if(split[0].equals(Integer.toString(errorCode))){
-					
+					this.errorMessage=split[1];
+					temp = false;
 				}
+			}
+			bReader.close();
+			if(temp){
+				this.errorMessage="Unbekannter Fehler";
 			}
 		} catch (IOException e) {
 			
@@ -114,10 +45,6 @@ public class ErrorMessage {
 		
 	}
 	
-	private ErrorMessage(int errorCode, String errorMessage){
-		this.errorCode=errorCode;
-		this.errorMessage=errorMessage;
-	}
 	
 	public ErrorMessage(){
 		this.errorMessage=null;
--- WebShopR4F/src/r4f/model/TestInsert.java
+++ WebShopR4F/src/r4f/model/TestInsert.java
@@ -6,13 +6,16 @@ public class TestInsert {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		DatenbankVerbindung db = new DatenbankVerbindung();
-				
-		Benutzer b = new Benutzer(1, "vorname", "nachname", "email3@test.com", new Date(1),  "password", "strasse", "hausnummer", "12345", "Stadt", "Herr");
+//		DatenbankVerbindung db = new DatenbankVerbindung();
+//				
+//		Benutzer b = new Benutzer(1, "vorname", "nachname", "email3@test.com", new Date(1),  "password", "strasse", "hausnummer", "12345", "Stadt", "Herr");
+//		
+//		db.CreateBenutzerInDB(b);
+//		b = db.getBenutzer("email3@test.com");
+//		System.out.println(b.getId());
 		
-		db.CreateBenutzerInDB(b);
-		b = db.getBenutzer("email3@test.com");
-		System.out.println(b.getId());
+		ErrorMessage error = new ErrorMessage(104);
+		System.out.println(error.getErrorMessage());
 		
 	}
 
