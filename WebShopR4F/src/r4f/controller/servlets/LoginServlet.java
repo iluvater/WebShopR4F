45fc2a5f070b4ced09bdd3179fc523c15c6f80db
iluvater <ture.kuesener@gmx.de>
2017-12-09 17:02:37 +0100

    Update LoginServlet.java

--- WebShopR4F/src/r4f/controller/servlets/LoginServlet.java
+++ WebShopR4F/src/r4f/controller/servlets/LoginServlet.java
@@ -64,7 +64,7 @@ public class LoginServlet extends HttpServlet {
 		Article article;
 
 		String errorURL = "Login.jsp";
-		String successURL = "./NavigationOverviewSerlvet";
+		String successURL = "./NavigationOverviewServlet";
 		String shoppingBasketURL = "./AddToShoppingBasketServlet";
 		String wishlistURL = "./AddToWishlistServlet";
 
